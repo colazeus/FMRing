22c984c6a76200862596bf9d8682fe954f1b460d
colazeus <xiaoxuan199@gmail.com>
2014-03-20 23:18:10 +0800

    change

--- PlayerCore.py
+++ PlayerCore.py
@@ -5,6 +5,8 @@ global player
 global isPlayerOn
 global songTimer
 global nowPlayingSong
+global userInfo
+isLike = False
 isPlayerOn = False
 songQueue = Queue.Queue(0)
 
@@ -19,24 +21,25 @@ def login():
 
 def getSongs(user_id,token,expire):
 	httpConnection = httplib.HTTPConnection('www.douban.com')
-	httpConnection.request('GET','/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id='+user_id+'&token='+token+'&expire='+expire+'&channel=0&type=n&kbps=192')
+	httpConnection.request('GET','/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id='+user_id+'&token='+token+'&expire='+expire+'&channel=0&type=n')
 	response = httpConnection.getresponse()
 	print('get songlist success')
 	return json.loads(response.read())
 
 def reloadSongQueue():
 	global songQueue
+	global userInfo
 	userInfo = login()
 	songlist = getSongs(userInfo['user_id'],userInfo['token'],userInfo['expire'])['song']
 	songQueue = Queue.Queue(0)
-	songQueue.put(songlist[0])
-	songQueue.put(songlist[1])
-	songQueue.put(songlist[2])
-	songQueue.put(songlist[3])
-	songQueue.put(songlist[4])
+	if len(songlist) > 1:
+		songQueue.put(songlist[0])
+		songQueue.put(songlist[1])
+		songQueue.put(songlist[2])
+		songQueue.put(songlist[3])
+		songQueue.put(songlist[4])
 
 def endSong():
-	global player
 	playNext()
 
 def play():
@@ -45,6 +48,7 @@ def play():
 	global songTimer
 	global songQueue
 	global nowPlayingSong
+	global isLike
 
 	if isPlayerOn == False:
 		if songQueue.qsize() == 0:
@@ -55,6 +59,10 @@ def play():
 		songTimer = Timer(song['length'],endSong)
 		songTimer.start()
 		isPlayerOn = True
+		if song['like'] == 1:
+			isLike = True
+		else:
+			isLike = False
 		print "Player On:",song['title']
 	else:
 		songTimer.cancel()
@@ -65,7 +73,9 @@ def play():
 
 def ring():
 	global isPlayerOn
+	
 	if isPlayerOn == False:
+		reloadSongQueue()
 		play()
 
 def playNext():
@@ -83,3 +93,31 @@ def getInfo():
 	else:
 		return nowPlayingSong
 
+def likeTheSong():
+	global nowPlayingSong
+	global userInfo
+	global isLike
+	if isPlayerOn == True:
+		httpConnection = httplib.HTTPConnection('www.douban.com')
+		if isLike == False:
+			httpConnection.request('GET','/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id='+userInfo['user_id']+'&token='+userInfo['token']+'&expire='+userInfo['expire']+'&channel=0&type=r&sid='+nowPlayingSong['sid'])
+			httpConnection.getresponse()
+			print "Like the song:",nowPlayingSong['title']
+			isLike = True
+			return True
+		else:
+			httpConnection.request('GET','/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id='+userInfo['user_id']+'&token='+userInfo['token']+'&expire='+userInfo['expire']+'&channel=0&type=u&sid='+nowPlayingSong['sid'])
+			httpConnection.getresponse()
+			print "Unlike the song:",nowPlayingSong['title']
+			isLike = False
+			return False
+
+def delTheSong():
+	global nowPlayingSong
+	global userInfo
+	if isPlayerOn == True:
+		httpConnection = httplib.HTTPConnection('www.douban.com')
+		httpConnection.request('GET','/j/app/radio/people?app_name=radio_desktop_win&version=100&user_id='+userInfo['user_id']+'&token='+userInfo['token']+'&expire='+userInfo['expire']+'&channel=0&type=b&sid='+nowPlayingSong['sid'])
+		httpConnection.getresponse()
+		print "Del the song:",nowPlayingSong['title']
+		playNext()
--- Ring_Server.py
+++ Ring_Server.py
@@ -1,7 +1,7 @@
-import subprocess,urllib,httplib,json,time,thread,PlayerCore
+import subprocess,urllib,httplib,json,time,thread,PlayerCore,string
 from SocketServer import TCPServer,StreamRequestHandler,ThreadingMixIn
 
-ring_time = "09-00"
+ring_time = "13:44:30"
 
 class Server(TCPServer,ThreadingMixIn):pass
 
@@ -23,15 +23,23 @@ class Handler(StreamRequestHandler):
                 info = PlayerCore.getInfo()
                 jsonInfo = json.dumps(info)
                 self.wfile.write(jsonInfo)
+            elif data == "Like":
+                like = PlayerCore.likeTheSong()
+            elif data == "Del":
+                info = PlayerCore.delTheSong()
+            elif "SetRing:" in data:
+                time = string.replace(data,"SetRing:",1);
+                ring_time = time;
+                print "Set Ring:",ring_time
 
 def ringer():
     while True:
         global ring_time
-        now_time = time.strftime('%H-%M',time.localtime(time.time()))
+        now_time = time.strftime('%H:%M:%S',time.localtime(time.time()))
         if ring_time == now_time:
             print "It's time"
             PlayerCore.ring()
-        time.sleep(10)
+        time.sleep(1)
 
 thread.start_new_thread(ringer,())
 
